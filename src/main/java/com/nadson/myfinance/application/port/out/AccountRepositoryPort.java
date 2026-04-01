03ce5ed91ebd0121dcb12a91c8dc7e68e9f7184b
Njansh <nadsonjhonyobmep@gmail.com>
2026-04-01 03:36:35 -0300

    feat: add transaction use case and ports with account existence check

--- src/main/java/com/nadson/myfinance/application/port/out/AccountRepositoryPort.java
+++ src/main/java/com/nadson/myfinance/application/port/out/AccountRepositoryPort.java
@@ -2,6 +2,10 @@ package com.nadson.myfinance.application.port.out;
 
 import com.nadson.myfinance.domain.entity.Account;
 
+import java.util.UUID;
+
 public interface AccountRepositoryPort {
-    public Account save(Account account);
+     Account save(Account account);
+
+    Account findById(UUID accountId);
 }
