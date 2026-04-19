daa9c623d67c397efd8fa9c07f22953f0bba3096
Njansh <nadsonjhonyobmep@gmail.com>
2026-04-19 13:59:47 -0300

    fix: ensure total calculation in reports by using findAll to ignore pagination"e descrição na listagem de transações

--- src/main/java/com/nadson/myfinance/application/port/in/GetTransactionPort.java
+++ src/main/java/com/nadson/myfinance/application/port/in/GetTransactionPort.java
@@ -2,8 +2,10 @@ package com.nadson.myfinance.application.port.in;
 
 import com.nadson.myfinance.domain.entity.Transaction;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface GetTransactionPort {
     Transaction execute(UUID accountId);
+    List<Transaction> execute(UUID accountId, String description);
 }
