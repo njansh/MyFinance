3bd9be0c021198794f41e7ae1b1d9e6eb404ed88
Njansh <nadsonjhonyobmep@gmail.com>
2026-04-25 09:20:49 -0300

    feat: add interactive dashboard and CORS configuration

---
+++ src/main/java/com/nadson/myfinance/infrastructure/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.nadson.myfinance.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Permite que o seu HTML aceda aos endpoints
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
